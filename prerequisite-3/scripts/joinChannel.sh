cab654cee121533d34258957fa6d9f2a12dffa9b
Vishva066 <vishvaprasad.p.2019.cse@rajalakshmi.edu.in>
2024-10-05 21:10:29 +0530

    script changes

--- prerequisite-3/scripts/joinChannel.sh
+++ prerequisite-3/scripts/joinChannel.sh
@@ -1,6 +1,6 @@
 export CHANNEL_NAME=autochannel
 CORE_PEER_LOCALMSPID=$1
 
-peer channel join -b ${PWD}/channel-artifacts/${CHANNEL_NAME}.block
+peer channel join -b /channel-artifacts/${CHANNEL_NAME}.block
 
 peer channel list
